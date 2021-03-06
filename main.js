4f171f339c7872908dbd3e0c679e48164e335882
Dakskihedron <ghasthunter352@gmail.com>
2021-03-06 23:19:00 +1300

    Added browser colour scheme detection

--- main.js
+++ main.js
@@ -37,3 +37,9 @@ function baseGenerate() {
 function toggleDark() {
     document.body.classList.toggle('dark');
 }
+
+function checkDark() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.classList.toggle('dark');
+    } 
+}
